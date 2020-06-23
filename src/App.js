05cd306141012cf371821a5bf7f57c743406948a
NotoriousCottonball <gpianka1@gmail.com>
2020-06-22 23:31:48 -0700

    Add Node-Sass

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from './KetoCafe.jpg';
 import './App.css';
+import HomePage from './components/homepage.js';
+
 
 function App() {
   return (
     <div>
+        <HomePage />
         <img src={logo} className="App-logo" alt="logo" />
     </div>
   );
