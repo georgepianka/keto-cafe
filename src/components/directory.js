6be199674a3cc355fb528558436961ae5f5ad6cb
NotoriousCottonball <gpianka1@gmail.com>
2020-06-25 14:21:53 -0700

    Style Logo

--- src/components/directory.js
+++ src/components/directory.js
@@ -12,22 +12,22 @@ class Directory extends React.Component {
       sections: [
         {
           title: 'beverages',
-          imageUrl: 'https://lowcarbyum.com/wp-content/uploads/2012/08/low-carb-zucchini-bread-l.jpg',
+          imageUrl: 'https://ohsodelicioso.com/wp-content/uploads/2014/09/Snickerdoodleproteinshake-4.jpg',
           id: 1
         },
         {
-          title: 'supplements',
-          imageUrl: 'https://lowcarbyum.com/wp-content/uploads/2012/08/low-carb-zucchini-bread-l.jpg',
-          id: 2
-        },
-        {
           title: 'snacks',
-          imageUrl: 'https://lowcarbyum.com/wp-content/uploads/2012/08/low-carb-zucchini-bread-l.jpg',
+          imageUrl: 'https://realsimplegood.com/wp-content/uploads/Paleo-spiced-nuts-two.jpg',
           id: 3
         },
         {
+          title: 'supplements',
+          imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSj0NEf46RJJVoDOgbPCgHHRbi4RhPbG_zQsUBNEJgeJfv-1ANQ&usqp=CAU',
+          id: 2
+        },
+        {
           title: 'meals',
-          imageUrl: 'https://lowcarbyum.com/wp-content/uploads/2012/08/low-carb-zucchini-bread-l.jpg',
+          imageUrl: 'https://www.happyveggiekitchen.com/wp-content/uploads/2020/04/Halloumi-Black-Bean-Salad-6.jpg',
           size: 'large',
           id: 4
         },
