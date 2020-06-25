740dfa87c1397e5f1acda4e27b79f9425f0899ca
NotoriousCottonball <gpianka1@gmail.com>
2020-06-25 16:24:37 -0700

    Setup React Router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './KetoCafe.png';
 import './App.css';
 import HomePage from './containers/homepage.js';
+import { Switch, Route } from 'react-router-dom';
 
 
 function App() {
@@ -9,6 +10,24 @@ function App() {
     <div>
         <img src={logo} className="logo" alt="logo" />
         <HomePage />
+    </div>
+  );
+}
+
+
+const MealsPage = () => (
+  <div>
+    <h1>MEALS PAGE </h1>
+  </div>
+);
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/meals' component={MealsPage} />
+      </Switch>
     </div>
   );
 }
