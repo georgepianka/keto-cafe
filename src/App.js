4b4d60e53caf158d8c8c8459cfbdeb645869b2b5
NotoriousCottonball <gpianka1@gmail.com>
2020-06-25 22:04:35 -0700

    Add shop component and route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import logo from './KetoCafe.png';
 import './App.css';
 import HomePage from './containers/homepage.js';
-import { Switch, Route } from 'react-router-dom';
+import Shop from './containers/shop.js';
+
 
 
 const MealsPage = () => (
@@ -17,6 +19,7 @@ function App() {
       <img src={logo} className="logo" alt="logo" />
       <Switch>
         <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={Shop} />
         <Route path='/meals' component={MealsPage} />
       </Switch>
     </div>
