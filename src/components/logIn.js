1408b390f326b1e1bc2e8fcc022269839bb9f1cd
NotoriousCottonball <gpianka1@gmail.com>
2020-06-26 23:04:59 -0700

    Create logIn component

---
+++ src/components/logIn.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import FormInput from './formInput.js';
+import CustomButton from './customButton.js';
+
+import '../styles/log-in.scss';
+
+class LogIn extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    this.setState({ email: '', password: '' });
+  };
+
+  handleChange = event => {
+    const { value, name } = event.target;
+
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className='log-in'>
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name='email'
+            type='email'
+            handleChange={this.handleChange}
+            value={this.state.email}
+            label='email'
+            required
+          />
+          <FormInput
+            name='password'
+            type='password'
+            value={this.state.password}
+            handleChange={this.handleChange}
+            label='password'
+            required
+          />
+          <CustomButton type='submit'> Sign in </CustomButton>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LogIn;
