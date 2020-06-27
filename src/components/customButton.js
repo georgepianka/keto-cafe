7757650efa267516e2727881106b10d5762edea6
NotoriousCottonball <gpianka1@gmail.com>
2020-06-26 21:37:00 -0700

    Create customButton component and stylesheet

---
+++ src/components/customButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import '../styles/custom-button.scss';
+
+const CustomButton = ({ children, ...otherProps }) => (
+  <button className='custom-button' {...otherProps}>
+    {children}
+  </button>
+);
+
+export default CustomButton;
