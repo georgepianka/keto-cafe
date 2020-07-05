2efdc29a4ed69bec67dd97d45ba4d89de8a11cc7
NotoriousCottonball <gpianka1@gmail.com>
2020-07-04 19:09:39 -0700

    Configure firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,4 +1,8 @@
-{
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
     apiKey: "AIzaSyCYd_ogCTMz6h2I05GaKlBP0GFSt92y6f0",
     authDomain: "keto-cafe.firebaseapp.com",
     databaseURL: "https://keto-cafe.firebaseio.com",
@@ -7,4 +11,16 @@
     messagingSenderId: "390867169540",
     appId: "1:390867169540:web:0a1f1630390b1e2504a051",
     measurementId: "G-PRNTD8X6DY"
-  }
+  };
+
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
