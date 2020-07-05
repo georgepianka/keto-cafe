d84477adc8c8a3ea0f16ebc772c8aae4b269d5dd
NotoriousCottonball <gpianka1@gmail.com>
2020-07-04 19:05:50 -0700

    Add firebase library and firebase.utils.js

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,10 @@
+{
+    apiKey: "AIzaSyCYd_ogCTMz6h2I05GaKlBP0GFSt92y6f0",
+    authDomain: "keto-cafe.firebaseapp.com",
+    databaseURL: "https://keto-cafe.firebaseio.com",
+    projectId: "keto-cafe",
+    storageBucket: "keto-cafe.appspot.com",
+    messagingSenderId: "390867169540",
+    appId: "1:390867169540:web:0a1f1630390b1e2504a051",
+    measurementId: "G-PRNTD8X6DY"
+  }
