df8782f45d48b3444be643225d7024bdbae8a507
NotoriousCottonball <gpianka1@gmail.com>
2020-07-06 18:40:27 -0700

    Configure currentUser state change for google authorization

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 import HomePage from './pages/homepage.js';
 import Shop from './pages/shop.js';
 import Header from './components/header.js';
-import LogInAndSignUp from './pages/logInAndSignUp.js'
+import LogInAndSignUp from './pages/logInAndSignUp.js';
+import { auth } from './firebase/firebase.utils.js';
 
 
 
@@ -14,19 +15,43 @@ const MealsPage = () => (
   </div>
 );
 
-function App() {
-  return (
-    <div>
-      <Header />
+class App extends React.Component {
+  constructor() {
+    super();
 
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={Shop} />
-        <Route path='/meals' component={MealsPage} />
-        <Route path='/login' component={LogInAndSignUp} />
-      </Switch>
-    </div>
-  );
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={Shop} />
+          <Route path='/meals' component={MealsPage} />
+          <Route path='/login' component={LogInAndSignUp} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
