7f75c3678b641dc24446f3aa2e6394bff92671e6
NotoriousCottonball <gpianka1@gmail.com>
2020-07-06 19:51:28 -0700

    Add SignUp to logInAndSignUp page

--- src/pages/logInAndSignUp.js
+++ src/pages/logInAndSignUp.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import LogIn from '../components/logIn.js';
+import SignUp from '../components/signUp.js';
 
 import '../styles/log-in-and-sign-up.scss';
+
 
 const LogInAndSignUp = () => (
   <div className='log-in-and-sign-up'>
     <LogIn />
+    <SignUp />
   </div>
 );
 
