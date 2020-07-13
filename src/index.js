d0ffeb123a35abb97dec15036dcf4f2ff9e1da89
NotoriousCottonball <gpianka1@gmail.com>
2020-07-12 20:15:01 -0700

    Configure redux store

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 //import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
+
+  <Provider store={store}>
    <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
