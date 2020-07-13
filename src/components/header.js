49d248d4b6378cb75f5deda5227e762751daa677
NotoriousCottonball <gpianka1@gmail.com>
2020-07-12 20:36:50 -0700

    Connect header component to redux store

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import logo from '../KetoCafe.png';
 
@@ -32,4 +33,8 @@ const Header = ({ currentUser }) => (
   </div>
 );
 
-export default Header;
+export default connect(
+  state => ({
+  currentUser: state.user.currentUser
+  })
+) (Header);
