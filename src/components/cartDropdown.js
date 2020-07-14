3666b88970c9d672c165ee74bac4b61f3acc54a8
NotoriousCottonball <gpianka1@gmail.com>
2020-07-13 22:07:33 -0700

    Integrate cartItem with cartDropdown

--- src/components/cartDropdown.js
+++ src/components/cartDropdown.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import CartItem from './cartItem.js';
 
 import CustomButton from './customButton.js';
 
 import '../styles/cart-dropdown.scss';
 
-const CartDropdown = () => (
+const CartDropdown = ({cartItems}) => (
   <div className='cart-dropdown'>
-    <div className='cart-items' />
+    <div className='cart-items'>
+      {cartItems.map(item => (
+        <CartItem key={item.id} item={item} />
+      ))}
+    </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
 
-export default CartDropdown;
+export default connect(
+  ({ cart: { cartItems } }) => ({
+  cartItems
+  })
+)(CartDropdown);
