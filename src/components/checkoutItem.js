c66ab135f0bb6c2791c0b614ecfcfe4b31205914
NotoriousCottonball <gpianka1@gmail.com>
2020-07-21 22:54:09 -0700

    Finish increase/reduce functionality for checkoutItem

--- src/components/checkoutItem.js
+++ src/components/checkoutItem.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { removeItemFromCart } from '../redux/cart/cartActions.js'
+import { addItem, removeItemFromCart, reduceItemInCart } from '../redux/cart/cartActions.js'
 
 import '../styles/checkout-item.scss';
 
-const CheckoutItem = ({ cartItem, cartItem: { name, imageUrl, price, quantity }, removeItemFromCart }) => (
+const CheckoutItem = ({ cartItem, cartItem: { name, imageUrl, price, quantity }, removeItemFromCart, addItem, reduceItemInCart }) => (
   <div className='checkout-item'>
     <div className='image-container'>
       <img src={imageUrl} alt='item' />
     </div>
     <span className='name'>{name}</span>
-    <span className='quantity'>{quantity}</span>
+
+    <span className='quantity'>
+        <div className='arrow' onClick={() => reduceItemInCart(cartItem)}>
+          &#10094;
+        </div>
+        <span className='value'>{quantity}</span>
+        <div className='arrow' onClick={() => addItem(cartItem)}>
+          &#10095;
+        </div>
+    </span>
+
     <span className='price'>{price}</span>
     <div className='remove-button' onClick={() => removeItemFromCart(cartItem)}>
     &#10005;
@@ -25,5 +35,5 @@ const CheckoutItem = ({ cartItem, cartItem: { name, imageUrl, price, quantity },
 
 export default connect(
   null,
-  {removeItemFromCart}
+  {removeItemFromCart, addItem, reduceItemInCart}
 )(CheckoutItem);
