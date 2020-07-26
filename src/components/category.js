05206f800a4638990051eb3da76cb48076af518f
NotoriousCottonball <gpianka1@gmail.com>
2020-07-25 17:28:38 -0700

    Create category component/stylesheet

---
+++ src/components/category.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import CategoryItem from './categoryItem.js';
+
+import { selectCategory } from '../redux/shop/shopSelectors.js';
+
+import '../styles/category.scss';
+
+const CategoryPage = ({ category }) => {
+  const { title, items } = category;
+  return (
+    <div className='category-page'>
+      <h2 className='title'>{title}</h2>
+      <div className='items'>
+        {items.map(item => (
+          <CategoryItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = (state, ownProps) => ({
+  category: selectCategory(ownProps.match.params.categoryId)(state)
+});
+
+export default connect(mapStateToProps)(CategoryPage);
