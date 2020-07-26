93410e192f888dc847820454b4f147161ece04b2
NotoriousCottonball <gpianka1@gmail.com>
2020-07-25 19:05:30 -0700

    Configure categoryPreview links to categoryPage

--- src/components/categoryPreview.js
+++ src/components/categoryPreview.js
@@ -1,20 +1,23 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import CategoryItem from './categoryItem.js';
 
 import '../styles/category-preview.scss';
 
-const CategoryPreview = ({ title, items }) => (
+const CategoryPreview = ({ title, items, match, history }) => (
   <div className='category-preview'>
-    <h1 className='title'>{title.toUpperCase()}</h1>
+    <h1 className='title' onClick={() => history.push(`${match.url}/${title.toLowerCase()}`)}>
+    {title.toUpperCase()}
+    </h1>
     <div className='preview'>
       {items
         .filter((item, idx) => idx < 4)
         .map(item => (
           <CategoryItem key={item.id} item={item} />
-        ))}
+        ))} 
     </div>
   </div>
 );
 
-export default CategoryPreview;
+export default withRouter(CategoryPreview);
