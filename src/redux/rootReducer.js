78de733daa6c6820dc73bcd35f9792b3b8434a9b
NotoriousCottonball <gpianka1@gmail.com>
2020-07-25 17:13:28 -0700

    Add directoryReducer/shopReducer to rootReducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -4,6 +4,8 @@ import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/userReducer.js';
 import cartReducer from './cart/cartReducer.js';
+import directoryReducer from './directory/directoryReducer.js';
+import shopReducer from './shop/shopReducer.js';
 
 
 const persistConfig = {
@@ -14,7 +16,9 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);
